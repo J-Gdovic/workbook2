e2cb26d785c31db67ab52897b6afcea7b7ea99aa
J Gdovic <jgdovic3@gmail.com>
2023-10-18 13:48:39 -0400

    taxes clean and distiance

---
+++ distance.js
@@ -0,0 +1,13 @@
+"use strict"
+//inputs
+ var x1 = 5;
+ var y1 = 6;
+ var x2 = 3;
+ var y2 = 9;
+
+ //calculation
+ var distance = Math.sqrt((x2-x1)**2 + (y2-y1)**2)
+ 
+
+//output
+ console.log(distance)
--- taxes.js
+++ taxes.js
@@ -4,17 +4,14 @@ var federalTax = 23 / 100;
 var yearlySalary = 55000;
 var monthsInAYear = 12;
 
-//calculations and
-console.log(yearlySalary / monthsInAYear);
+//calculations
 
-var salaryPerMonth = 4583.333333333333;
+var salaryPerMonth = yearlySalary / monthsInAYear;
 
-console.log(federalTax * salaryPerMonth);
+var taxWitheldPerMonth = federalTax * salaryPerMonth;
 
-var taxWitheldPerMonth = 1054.1666666666667;
+var taxWhitheldPerYear = taxWitheldPerMonth * monthsInAYear;
 
-console.log(taxWitheldPerMonth * monthsInAYear);
-
-var taxWhitheldPerYear = 12650;
+//output
 
 console.log("With 23% of taxes witheld of a salary of " + yearlySalary + " is " + taxWhitheldPerYear.toFixed(2));
