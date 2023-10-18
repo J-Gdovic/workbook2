96d91ac53c5bec6048e44d12a2db041e684f1a7b
J Gdovic <jgdovic3@gmail.com>
2023-10-17 22:27:43 -0400

    rule of 72

--- rule_of_72.js
+++ rule_of_72.js
@@ -1,4 +1,14 @@
 //inputs
 
-var savingsAccount;
-var interestRates;
+var savingsAccount = 500000;
+var interestRates = 5;
+var ruleOf72 = 72;
+
+//calculations
+console.log(ruleOf72 / interestRates);// years : 14.4
+console.log(savingsAccount * 2) //savings doubled: 1000000
+
+ 
+
+//output
+console.log("At a 5% interest rate, your savings account will be worth " + 2 *  savingsAccount.toFixed(2)  + " in  14.4 years ");
