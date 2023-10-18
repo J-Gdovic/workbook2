a7d6d8bd06eb0253c0e8baabad2b03ae2bf57ea4
J Gdovic <jgdovic3@gmail.com>
2023-10-18 10:15:58 -0400

    72 and tip amount adjustments

--- rule_of_72.js
+++ rule_of_72.js
@@ -5,10 +5,10 @@ var interestRates = 5;
 var ruleOf72 = 72;
 
 //calculations
-console.log(ruleOf72 / interestRates);// years : 14.4
-console.log(savingsAccount * 2) //savings doubled: 1000000
+//console.log(ruleOf72 / interestRates);// years : 14.4
+//console.log(savingsAccount * 2); //savings doubled: 1000000
 
  
 
 //output
-console.log("At a 5% interest rate, your savings account will be worth " + 2 *  savingsAccount.toFixed(2)  + " in  14.4 years ");
+console.log("At a 5% interest rate, your savings account will be worth " + 2 *  (savingsAccount.toFixed(2))  + " in  14.4 years ");
--- tip_amouont.js
+++ tip_amouont.js
@@ -1,6 +1,6 @@
 //inputs
 var billTotal = 70.00
-var tipPercentage = 0.20
+var tipPercentage = 0.2
 
 
 //calculation
@@ -12,4 +12,4 @@ var totalAndTip = (billTotal + tip)
 //totalAndTip = 84
 //outputs
 console.log(tip)
-console.log("The tip on  a $70 food bill with a tip percentage of 20 is : " + totalAndTip.toFixed(2))
+console.log("The tip on  a $" + billTotal + " food bill with a tip percentage of " + tipPercentage + " is : " + totalAndTip.toFixed(2))
